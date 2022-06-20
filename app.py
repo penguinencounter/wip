64506566bf6df07495c7f5315e8975613fb50f4b
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-06-20 12:30:35 -0700

    copy p5 too pls

--- app.py
+++ app.py
@@ -52,15 +52,21 @@ def run_args(args: list):
         for cwd, _, f in os.walk('out'):
             for fp in f:
                 if fp.endswith('.html') or fp.endswith('.mjs'):
-                    print(f'Patching {fp} in {cwd}')
+                    print(f'Patching {fp} in {cwd}... ', flush=True, end='')
                     with open(os.path.join(cwd, fp), 'r') as f:
                         content = f.read()
                     oldsize = len(content)
                     content = re.sub(r"(['\"])(/static/.*)\1", fr"\1{base}\2\1", content)
-                    print(f'{fp}: {oldsize} -> {len(content)} char, +{len(content)/oldsize-1:.2%}')
+                    print(f'{oldsize} -> {len(content)} char, +{len(content)/oldsize-1:.2%}... ', flush=True, end='')
                     with open(os.path.join(cwd, fp), 'w') as f:
                         f.write(content)
+                    print(' done.', flush=True)
+                else:
+                    print(f'Reading {fp} (in {cwd})... ', end='', flush=True)
+                    with open(os.path.join(cwd, fp), 'rb') as f:
+                        content = f.read()
                     files[os.path.join(cwd, fp)] = content
+                    print(' done.', flush=True)
         if safe_to_switch():
             print('Checking out publishing branch `pages-static-build`...')
             subprocess.run(shlex.split('git checkout pages-static-build'))
@@ -72,8 +78,12 @@ def run_args(args: list):
                 print(f'write {len(content)} char to {fp2}')
                 if os.sep in fp2:
                     os.makedirs(os.sep.join(os.path.split(fp2)[:-1]), exist_ok=True)
-                with open(fp2, 'w') as f:
-                    f.write(content)
+                if type(content) == bytes:
+                    with open(fp2, 'wb') as f:
+                        f.write(content)
+                else:
+                    with open(fp2, 'w') as f:
+                        f.write(content)
             print('Committing...')
             subprocess.run(shlex.split('git add .'))
             subprocess.run(shlex.split('git commit -m "Publish static files: {}"'.format(time.asctime())))
